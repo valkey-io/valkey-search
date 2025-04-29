43c7101ed5963569f72e292580722f168113b375
yairgott <yairgott@gmail.com>
2025-04-29 02:05:44 +0000

    addressing code review comments
    Signed-off-by: yairgott <yairgott@gmail.com>

--- vmsdk/src/blocked_client.cc
+++ vmsdk/src/blocked_client.cc
@@ -29,6 +29,7 @@
 
 #include "vmsdk/src/blocked_client.h"
 
+#include "absl/base/no_destructor.h"
 #include "absl/container/flat_hash_map.h"
 #include "absl/log/check.h"
 #include "absl/synchronization/mutex.h"
@@ -36,27 +37,37 @@
 
 namespace vmsdk {
 absl::Mutex blocked_clients_mutex;
-absl::flat_hash_map<unsigned long long, BlockedClientEntry> gBlockedClients;
+static absl::NoDestructor<
+    absl::flat_hash_map<unsigned long long, BlockedClientEntry>>
+    tracked_blocked_clients;
 // Used for testing
 absl::flat_hash_map<unsigned long long, BlockedClientEntry> &
 TrackedBlockedClients() {
-  return gBlockedClients;
+  return *tracked_blocked_clients;
 }
-
+// The engine supports storing only one set of callback information (callback
+// function, timeout, etc.) per client. If BlockedClient is called multiple
+// times for the same client with non-empty callback information, it issues
+// VM_BlockClient each time, causing the engine to overwrite the previous
+// callback information with the latest one. If BlockedClient is called multiple
+// times with empty callback information, only the first call triggers
+// VM_BlockClient, while subsequent calls simply increment an internal reference
+// count. VM_UnblockClient is only called once the reference count returns to
+// zero.
 BlockedClient::BlockedClient(RedisModuleCtx *ctx, bool handle_duplication) {
   if (handle_duplication) {
     unsigned long long tracked_client_id = RedisModule_GetClientId(ctx);
     if (tracked_client_id != 0) {
       absl::MutexLock lock(&blocked_clients_mutex);
-      auto it = gBlockedClients.find(tracked_client_id);
-      if (it == gBlockedClients.end()) {
+      auto it = TrackedBlockedClients().find(tracked_client_id);
+      if (it == TrackedBlockedClients().end()) {
         blocked_client_ =
             RedisModule_BlockClient(ctx, nullptr, nullptr, nullptr, 0);
         if (!blocked_client_) {
           return;
         }
         tracked_client_id_ = tracked_client_id;
-        gBlockedClients[tracked_client_id_] = {1, blocked_client_};
+        TrackedBlockedClients()[tracked_client_id_] = {1, blocked_client_};
         return;
       }
       tracked_client_id_ = tracked_client_id;
@@ -103,15 +114,15 @@ void BlockedClient::UnblockClient() {
   auto tracked_client_id = std::exchange(tracked_client_id_, 0);
   if (tracked_client_id) {
     absl::MutexLock lock(&blocked_clients_mutex);
-    auto itr = gBlockedClients.find(tracked_client_id);
-    CHECK(itr != gBlockedClients.end());
+    auto itr = TrackedBlockedClients().find(tracked_client_id);
+    CHECK(itr != TrackedBlockedClients().end());
     auto &cnt = itr->second.cnt;
     CHECK_GT(cnt, 0);
     --cnt;
     if (cnt > 0) {
       return;
     }
-    gBlockedClients.erase(tracked_client_id);
+    TrackedBlockedClients().erase(tracked_client_id);
   }
   RedisModule_UnblockClient(blocked_client, private_data);
 }

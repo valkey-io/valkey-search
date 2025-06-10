5c97b47a258e532fccd07472eb80c2fc4e8ab88f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-06-10 10:48:07 -0400

    spelling: integration
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ RUN_TEST=""
 RUN_BUILD="yes"
 DUMP_TEST_ERRORS_STDOUT="no"
 NINJA_TOOL="ninja"
-INTEGRETION_TEST="no"
+INTEGRATION_TEST="no"
 ASAN_BUILD="no"
 ARGV=$@
 
@@ -86,7 +86,7 @@ do
         echo "Running test ${RUN_TEST}"
         ;;
     --run-integration-tests)
-        INTEGRETION_TEST="yes"
+        INTEGRATION_TEST="yes"
         shift || true
         echo "Running integration tests"
         ;;
@@ -288,7 +288,7 @@ elif [ ! -z "${RUN_TEST}" ]; then
     print_test_prefix "${TESTS_DIR}/${RUN_TEST}"
     (${TESTS_DIR}/${RUN_TEST} && print_test_ok) || print_test_error_and_exit
     print_test_summary
-elif [[ "${INTEGRETION_TEST}" == "yes" ]]; then
+elif [[ "${INTEGRATION_TEST}" == "yes" ]]; then
     cd testing/integration
     params=""
     if [[ "${DUMP_TEST_ERRORS_STDOUT}" == "yes" ]]; then

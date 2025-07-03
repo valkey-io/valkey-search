9496249d83ba3cc8b814853201d1ba2ecdd77671
Miles Song <bodasong@amazon.com>
2025-07-03 17:53:47 +0000

    create info grpc request and test

---
+++ src/coordinator/info_converter.cc
@@ -0,0 +1,11 @@
+#include "src/coordinator/info_converter.h"
+
+namespace valkey_search::coordinator {
+
+std::unique_ptr<InfoIndexRequest> CreateInfoIndexRequest(const std::string& index_name) {
+  auto request = std::make_unique<InfoIndexRequest>();
+  request->set_index_name(index_name);
+  return request;
+}
+
+}  // namespace valkey_search::coordinator

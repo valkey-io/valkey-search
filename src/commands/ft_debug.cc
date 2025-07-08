6936695be29e0e8e50c847886456eaac55fd6f0c
Allen Samuels <allenss@amazon.com>
2025-07-08 05:10:36 +0000

    Respond to review
    Signed-off-by: Allen Samuels <allenss@amazon.com>

--- src/commands/ft_debug.cc
+++ src/commands/ft_debug.cc
@@ -6,39 +6,37 @@
  */
 
 #include <absl/base/no_destructor.h>
+
 #include "info.h"
 #include "src/commands/commands.h"
-
 #include "vmsdk/src/command_parser.h"
 #include "vmsdk/src/status/status_macros.h"
 
-extern vmsdk::module::Options options; // Declared in module_loader.cc
+extern vmsdk::module::Options options;  // Declared in module_loader.cc
 namespace valkey_search {
 
 enum SubCommands {
-    kInfoMetadata,
-    kInfoValues,
+  kShowInfo,
 };
 
-const absl::flat_hash_map<absl::string_view, SubCommands>
-    kDebugSubcommands({
-        {"INFO_METADATA", SubCommands::kInfoMetadata},
-        {"INFO_VALUES", SubCommands::kInfoValues},
-    });
+const absl::flat_hash_map<absl::string_view, SubCommands> kDebugSubcommands({
+    {"SHOW_INFO", SubCommands::kShowInfo},
+});
 
 absl::Status FTDebugCmd(ValkeyModuleCtx *ctx, ValkeyModuleString **argv,
                         int argc) {
   vmsdk::ArgsIterator itr{argv, argc};
-  itr.Next(); // Skip the command name
+  itr.Next();  // Skip the command name
   SubCommands subcommand;
-  VMSDK_RETURN_IF_ERROR(vmsdk::ParseEnumParam(subcommand, itr, &kDebugSubcommands));
+  VMSDK_RETURN_IF_ERROR(
+      vmsdk::ParseEnumParam(subcommand, itr, &kDebugSubcommands));
   switch (subcommand) {
-      case SubCommands::kInfoMetadata: return vmsdk::info_field::DumpInfoMetaData(ctx, itr, options);
-      case SubCommands::kInfoValues: return vmsdk::info_field::DumpInfoValues(ctx, itr, options);
-      default:
-          assert(false);
+    case SubCommands::kShowInfo:
+      return vmsdk::info_field::ShowInfo(ctx, itr, options);
+    default:
+      assert(false);
   }
   return absl::InvalidArgumentError("Unknown command");
 }
 
-}
+}  // namespace valkey_search

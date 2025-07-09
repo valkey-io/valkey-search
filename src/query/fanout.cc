3830ab4478cb25007ea2bc529d2b14f86b191a3b
Miles Song <bodasong@amazon.com>
2025-07-09 23:05:51 +0000

    rename to fanout template

--- src/query/fanout.cc
+++ src/query/fanout.cc
@@ -35,7 +35,7 @@
 #include "src/coordinator/util.h"
 #include "src/indexes/vector_base.h"
 #include "src/query/search.h"
-#include "src/query/broadcast_template.h"
+#include "src/query/fanout_template.h"
 #include "src/utils/string_interning.h"
 #include "valkey_search_options.h"
 #include "vmsdk/src/log.h"
@@ -302,7 +302,7 @@ absl::Status PerformSearchFanoutAsync(
 std::vector<FanoutSearchTarget> GetSearchTargetsForFanout(
     ValkeyModuleCtx *ctx) {
   std::cout << "Using get target template" << std::endl;
-  return BroadcastTemplate::GetTargets<FanoutSearchTarget>(
+  return FanoutTemplate::GetTargets<FanoutSearchTarget>(
       ctx,
       []() { return FanoutSearchTarget{.type = FanoutSearchTarget::Type::kLocal}; },
       [](const std::string& address) {

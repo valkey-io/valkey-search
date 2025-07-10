103a1df3e0cc0447e5e1f18e78f7d08adc37bb70
Miles Song <bodasong@amazon.com>
2025-07-10 18:21:56 +0000

    add InfoIndexPartition RPC and local index info query

--- src/coordinator/info_converter.cc
+++ src/coordinator/info_converter.cc
@@ -2,8 +2,8 @@
 
 namespace valkey_search::coordinator {
 
-std::unique_ptr<InfoIndexRequest> CreateInfoIndexRequest(const std::string& index_name) {
-  auto request = std::make_unique<InfoIndexRequest>();
+std::unique_ptr<InfoIndexPartitionRequest> CreateInfoIndexPartitionRequest(const std::string& index_name) {
+  auto request = std::make_unique<InfoIndexPartitionRequest>();
   request->set_index_name(index_name);
   return request;
 }

e689a4831c6675ac101d927f86f611928602e390
Karthik Subbarao <karthikrs2021@gmail.com>
2025-07-18 19:33:14 +0000

    Add unit tests
    Signed-off-by: Karthik Subbarao <karthikrs2021@gmail.com>

--- testing/ft_create_parser_test.cc
+++ testing/ft_create_parser_test.cc
@@ -573,7 +573,7 @@ INSTANTIATE_TEST_SUITE_P(
                           }}},
          },
          {
-             .test_name = "missing_vector_index",
+             .test_name = "happy_path_numeric_index_on_hash",
              .success = true,
              .command_str = "idx1 on HASH SChema hash_field1 as "
                             "hash_field11 numeric ",
@@ -583,6 +583,23 @@ INSTANTIATE_TEST_SUITE_P(
                             .identifier = "hash_field1",
                             .attribute_alias = "hash_field11",
                             .indexer_type = indexes::IndexerType::kNumeric,
+                        }}},
+         },
+        {
+             .test_name = "happy_path_tag_index_on_hash",
+             .success = true,
+             .command_str = "idx1 on HASH SCHEMA hash_field1 as "
+                            "hash_field11 tag ",
+            .tag_parameters = {{
+                 .separator = ",",
+                 .case_sensitive = false,
+             }},
+             .expected = {.index_schema_name = "idx1",
+                        .on_data_type = data_model::ATTRIBUTE_DATA_TYPE_HASH,
+                        .attributes = {{
+                            .identifier = "hash_field1",
+                            .attribute_alias = "hash_field11",
+                            .indexer_type = indexes::IndexerType::kTag,
                         }}},
          },
          {

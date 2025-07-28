e5a36fd24025b5fdce4e1a9974310f1c184039c4
Nivesh Tuwani <tuwanivu@amazon.com>
2025-07-28 23:40:18 +0000

    Changes made to the file structure and inheritancen for textIndex

--- src/indexes/text_index.h
+++ src/indexes/text_index.h
@@ -1,10 +1,15 @@
 #ifndef VALKEY_SEARCH_INDEXES_TEXT_TEXT_H_
 #define VALKEY_SEARCH_INDEXES_TEXT_TEST_H_
 
+#include "absl/container/flat_hash_map.h"
+#include "absl/container/flat_hash_set.h"
+#include "src/index_schema.pb.h"
+#include "src/indexes/text/radix_tree.h" 
+
 namespace valkey_search {
 namespace indexes {
 
-struct Text : public IndexBase {
+struct Text {
   // Constructor
   Text(const data_model& text_index_proto);
 

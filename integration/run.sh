477ee2cc620117358801d9400242fe999b34cd7b
Eran Ifrah <eifrah@amazon.com>
2025-08-07 00:26:24 +0300

    build.sh now runs both integration test suits
    Signed-off-by: Eran Ifrah <eifrah@amazon.com>

--- integration/run.sh
+++ integration/run.sh
@@ -4,11 +4,12 @@
 ROOT_DIR=$(readlink -f $(readlink -f $(dirname $0))/..)
 
 . ${ROOT_DIR}/scripts/common.rc
+setup_valkey_server
 
 LOG_NOTICE "Root directory is: ${ROOT_DIR}"
 
 function print_usage() {
-cat<<EOF
+    cat <<EOF
 Usage: run.sh [options...]
 
     --debug                 Run integration tests in debug mode.
@@ -28,8 +29,7 @@ function check_existence() {
 
 ## Parse command line arguments
 BUILD_CONFIG="release"
-while [ $# -gt 0 ]
-do
+while [ $# -gt 0 ]; do
     arg=$1
     case $arg in
     --debug)
@@ -47,7 +47,7 @@ do
         SAN_SUFFIX="-tsan"
         LOG_INFO "Assuming TSan build"
         ;;
-    --help|-h)
+    --help | -h)
         print_usage
         exit 0
         ;;
@@ -68,15 +68,7 @@ if [ -z "${MODULE_PATH}" ]; then
 fi
 check_existence "MODULE_PATH" "${MODULE_PATH}"
 
-# Check for valkey-server
-VALKEY_SERVER_PATH="${VALKEY_SERVER_PATH:=}"
-if [ -z "${VALKEY_SERVER_PATH}" ]; then
-    VALKEY_SERVER_PATH=$(which valkey-server||true)
-    if [ -z "${VALKEY_SERVER_PATH}" ]; then
-        LOG_ERROR "Could not find valkey-server in PATH nor in VALKEY_SERVER_PATH environment variable"
-        exit 1
-    fi
-fi
+setup_valkey_server
 check_existence "VALKEY_SERVER_PATH" "${VALKEY_SERVER_PATH}"
 
 LOG_INFO "VALKEY_SERVER_PATH => ${VALKEY_SERVER_PATH}"
@@ -98,62 +90,18 @@ function setup_python() {
 }
 
 function zap() {
-    echo "Zapping $1...";
-    pids=$(ps -ef|grep $1|grep -v grep |awk '{print $2;}');
-    for pid in $pids;
-    do
-        kill -9 $pid;
+    echo "Zapping $1..."
+    pids=$(ps -ef | grep $1 | grep -v grep | awk '{print $2;}')
+    for pid in $pids; do
+        kill -9 $pid
     done
-}
-
-function install_test_framework() {
-    local test_framework_url="https://github.com/valkey-io/valkey-test-framework"
-    local test_framework_path="${WD}/valkey-test-framework"
-
-    LOG_INFO "PIP_PATH => ${PIP_PATH}"
-    if [ -L "${WD}/valkeytestframework" ]; then
-        LOG_INFO "valkey-test-framework found: ${test_framework_path}"
-    else
-        if [ ! -d ${test_framework_path} ]; then
-            LOG_INFO "Cloning valkey-test-framework into ${test_framework_path}"
-            git clone "${test_framework_url}" ${test_framework_path}
-        fi
-        pushd ${test_framework_path}
-        local requirements_txt=${test_framework_path}/requirements.txt
-        if [ -f ${requirements_txt} ]; then
-            LOG_INFO "Installing requirements file"
-            ${PIP_PATH} install -r ${requirements_txt}
-            ${PIP_PATH} install --upgrade pytest
-        fi
-        popd
-        ln -sf ${test_framework_path}/src ${WD}/valkeytestframework
-    fi
-}
-
-function build_json_module() {
-    local json_url="https://github.com/valkey-io/valkey-json"
-    local json_repo_path="${WD}/valkey-json"
-    local json_module=${json_repo_path}/.build-release/src/libjson.${MODULE_EXT}
-    if [ -f "${json_module}" ]; then
-        LOG_INFO "Found JSON module: ${json_module}"
-    else
-        LOG_INFO "Cloning ${json_url} into ${json_repo_path}"
-        git clone "${json_url}" ${json_repo_path}
-        pushd ${json_repo_path}
-        mkdir .build-release
-        pushd .build-release
-        cmake .. -DCMAKE_BUILD_TYPE=Release
-        make -j$(nproc)
-        popd
-        popd
-    fi
-    JSON_MODULE_PATH=${json_module}
 }
 
 # Check for user provided JSON module path
 JSON_MODULE_PATH="${JSON_MODULE_PATH:=}"
 if [ -z "${JSON_MODULE_PATH}" ]; then
-    build_json_module
+    setup_json_module
+    JSON_MODULE_PATH=${VALKEY_JSON_PATH}
 fi
 LOG_INFO "JSON_MODULE_PATH => ${JSON_MODULE_PATH}"
 
@@ -163,7 +111,6 @@ install_test_framework
 # Export variables required by the test framework
 export MODULE_PATH=${MODULE_PATH}
 export VALKEY_SERVER_PATH=${VALKEY_SERVER_PATH}
-export PYTHONPATH=${WD}/valkeytestframework:${WD}
 export JSON_MODULE_PATH=${JSON_MODULE_PATH}
 export SKIPLOGCLEAN=1
 
@@ -177,4 +124,4 @@ fi
 
 zap valkey-server
 LOG_INFO "Running: ${PYTHON_PATH} -m pytest ${FILTER_ARGS} --capture=sys --cache-clear -v ${ROOT_DIR}/integration/"
-${PYTHON_PATH} -m pytest ${FILTER_ARGS} --log-cli-level=INFO --capture=sys --cache-clear -v ${ROOT_DIR}/integration/
+${PYTHON_PATH} -m pytest ${FILTER_ARGS} --capture=sys --cache-clear -v ${ROOT_DIR}/integration/

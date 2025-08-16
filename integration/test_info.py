bcf4a4340b28fe1af14a0302462b4d8bff25b538
proost <jwalag87@gmail.com>
2025-08-16 15:31:52 +0900

    test: fix broken test cases
    Signed-off-by: proost <jwalag87@gmail.com>

--- integration/test_info.py
+++ integration/test_info.py
@@ -35,6 +35,7 @@ class TestVSSBasic(ValkeySearchTestCaseBase):
             "search_hnsw_create_exceptions_count",
             "search_string_interning_store_size",
             "search_string_interning_memory_bytes",
+            "search_string_interning_memory_human", # less than 1KiB
             "search_vector_externing_entry_count",
             "search_vector_externing_hash_extern_errors",
             "search_vector_externing_generated_value_cnt",
@@ -53,8 +54,7 @@ class TestVSSBasic(ValkeySearchTestCaseBase):
         ]
 
         bytes_fields = [
-            "search_used_memory_human",
-            "search_string_interning_memory_human"
+            "search_used_memory_human"
         ]
         
         double_fields = [

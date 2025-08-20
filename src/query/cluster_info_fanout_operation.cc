4c9c51a6a74bbc40f24a95d1f4bc6dafbca8111c
Miles Song <bodasong@amazon.com>
2025-08-20 17:02:08 +0000

    retry enabled option
    Signed-off-by: Miles Song <bodasong@amazon.com>

--- src/query/cluster_info_fanout_operation.cc
+++ src/query/cluster_info_fanout_operation.cc
@@ -163,7 +163,7 @@ bool ClusterInfoFanoutOperation::ShouldRetry() {
 }
 
 bool ClusterInfoFanoutOperation::ShouldRetry() {
-  return false;
+  return !inconsistent_state_error_nodes.empty();
 }
 
 }  // namespace valkey_search::query::cluster_info_fanout

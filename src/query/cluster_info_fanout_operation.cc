dc1ab28d4b75eb9f888aab1952799bc2960c7b05
Miles Song <bodasong@amazon.com>
2025-08-26 23:41:36 +0000

    fix clang tidy
    Signed-off-by: Miles Song <bodasong@amazon.com>

--- src/query/cluster_info_fanout_operation.cc
+++ src/query/cluster_info_fanout_operation.cc
@@ -168,7 +168,8 @@ bool ClusterInfoFanoutOperation::ShouldRetry() {
 }
 
 bool ClusterInfoFanoutOperation::ShouldRetry() {
-  return !inconsistent_state_error_nodes.empty() || !communication_error_nodes.empty();
+  return !inconsistent_state_error_nodes.empty() ||
+         !communication_error_nodes.empty();
 }
 
 }  // namespace valkey_search::query::cluster_info_fanout

0c30af39c7c848b823f9d6f2672eda51d4b2ccad
Allen Samuels <allenss@amazon.com>
2025-08-29 01:48:51 +0000

    Fix formatting
    Signed-off-by: Allen Samuels <allenss@amazon.com>

--- vmsdk/src/valkey_module_api/valkey_module.h
+++ vmsdk/src/valkey_module_api/valkey_module.h
@@ -661,7 +661,7 @@ static const ValkeyModuleEvent
 #define VALKEYMODULE_CLIENTINFO_FLAG_TRACKING (1 << 3)
 #define VALKEYMODULE_CLIENTINFO_FLAG_UNIXSOCKET (1 << 4)
 #define VALKEYMODULE_CLIENTINFO_FLAG_MULTI (1 << 5)
-#define VALKEYMODULE_CLIENTINFO_FLAG_READONLY (1 << 6) /* Valkey 9 available */
+#define VALKEYMODULE_CLIENTINFO_FLAG_READONLY (1 << 6) /* Valkey 9 */
 
 /* Here we take all the structures that the module pass to the core
  * and the other way around. Notably the list here contains the structures

2c56c5f7c3478d8fc9e35919338de1205d60eca2
Miles Song <bodasong@amazon.com>
2025-09-11 17:34:44 +0000

    add comment for controlled boolean
    Signed-off-by: Miles Song <bodasong@amazon.com>

--- vmsdk/src/debug.cc
+++ vmsdk/src/debug.cc
@@ -21,6 +21,7 @@
 namespace vmsdk {
 namespace debug {
 
+// consider changing controlled boolean from static to inline type
 Controlled<bool> ForceRetry("ForceRetry", false);
 
 absl::Mutex pause_point_lock;

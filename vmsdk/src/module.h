4c977ff324f11094896361e6f7d9148bbbe037f0
Dmitry Polyakovsky <dmitrypol@gmail.com>
2025-10-16 18:17:44 +0000

    added utils::verifyLoadedOnlyOnce, calling it from VALKEY_MODULE
    Signed-off-by: Dmitry Polyakovsky <dmitrypol@gmail.com>

--- vmsdk/src/module.h
+++ vmsdk/src/module.h
@@ -23,11 +23,15 @@
   extern "C" {                                                              \
   int ValkeyModule_OnLoad(ValkeyModuleCtx *ctx, ValkeyModuleString **argv,  \
                           int argc) {                                       \
+    if (!vmsdk::verifyLoadedOnlyOnce()) {                                   \
+      VMSDK_LOG(NOTICE, ctx) << "Module cannot be loaded more than once";   \
+      return VALKEYMODULE_ERR;                                              \
+    }                                                                       \
+    vmsdk::TrackCurrentAsMainThread();                                      \
     if (auto status = vmsdk::module::OnLoad(ctx, argv, argc, options);      \
         status != VALKEYMODULE_OK) {                                        \
       return status;                                                        \
     }                                                                       \
-    vmsdk::TrackCurrentAsMainThread();                                      \
                                                                             \
     if (options.on_load.has_value()) {                                      \
       return vmsdk::module::OnLoadDone(                                     \

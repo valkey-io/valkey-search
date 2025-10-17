a785ca0c1de7de17de31f07766de923929715692
Dmitry Polyakovsky <dmitrypol@gmail.com>
2025-10-17 17:36:43 +0000

    format verifyLoadedOnlyOnce
    Signed-off-by: Dmitry Polyakovsky <dmitrypol@gmail.com>

--- vmsdk/src/utils.cc
+++ vmsdk/src/utils.cc
@@ -54,10 +54,10 @@ int StopTimerFromBackgroundThread(
 }
 
 bool verifyLoadedOnlyOnce() {
-    static bool prev_loaded = false;
-    if (prev_loaded) return false;
-    prev_loaded = true;
-    return true;
+  static bool prev_loaded = false;
+  if (prev_loaded) return false;
+  prev_loaded = true;
+  return true;
 }
 
 void TrackCurrentAsMainThread() {

d3059f3fc5544513f683493870088f75309cf0a0
Dmitry Polyakovsky <dmitrypol@gmail.com>
2025-10-20 16:08:00 +0000

    ran ./build.sh --format
    Signed-off-by: Dmitry Polyakovsky <dmitrypol@gmail.com>

--- vmsdk/src/module.cc
+++ vmsdk/src/module.cc
@@ -83,18 +83,19 @@ int OnLoad(ValkeyModuleCtx *ctx, ValkeyModuleString **argv, int argc,
     return VALKEYMODULE_ERR;
   }
   if (ValkeyModule_GetServerVersion == nullptr) {
-    VMSDK_LOG(WARNING, ctx) << "ValkeyModule_GetServerVersion function is not available";
+    VMSDK_LOG(WARNING, ctx)
+        << "ValkeyModule_GetServerVersion function is not available";
     return VALKEYMODULE_ERR;
   }
   auto server_version = ValkeyModule_GetServerVersion();
   if (server_version < options.minimum_valkey_version) {
     VMSDK_LOG(WARNING, ctx)
-      << "Minimum required server version is "
-      << vmsdk::DisplayValkeyVersion(options.minimum_valkey_version)
-      << ", Current version is "
-      << vmsdk::DisplayValkeyVersion(server_version);    
+        << "Minimum required server version is "
+        << vmsdk::DisplayValkeyVersion(options.minimum_valkey_version)
+        << ", Current version is "
+        << vmsdk::DisplayValkeyVersion(server_version);
     return VALKEYMODULE_ERR;
-  }  
+  }
   if (auto status = AddACLCategories(ctx, options.acl_categories);
       !status.ok()) {
     ValkeyModule_Log(ctx, VALKEYMODULE_LOGLEVEL_WARNING, "%s",

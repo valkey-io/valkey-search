0f0a6bd3c9adc6532c1f358e466bb86fea000fcb
VoletiRam <90425341+VoletiRam@users.noreply.github.com>
2025-11-01 08:06:55 -0700

    Integrate ICU library for multiple language support (#443)
    Integrate ICU library for multiple langugage support. We require unicode
    normalization and break iterators for boundary analysis and compound
    word breaking functionalities which are provided by ICU library for
    international languages. Statically linking ICU library which includes
    dictionary data pacakging and static building of ICU library. Provide
    APis we use for mulit language support. Build the components only
    required for full text search and disable at build level.
    
    Signed-off-by: Ram Prasad Voleti <ramvolet@amazon.com>
    Co-authored-by: Ram Prasad Voleti <ramvolet@amazon.com>

--- src/indexes/text/lexer.cc
+++ src/indexes/text/lexer.cc
@@ -11,6 +11,7 @@
 #include "absl/status/status.h"
 #include "absl/strings/ascii.h"
 #include "libstemmer.h"
+#include "src/indexes/text/unicode_normalizer.h"
 #include "src/utils/scanner.h"
 
 namespace valkey_search::indexes::text {
@@ -102,7 +103,7 @@ absl::StatusOr<std::vector<std::string>> Lexer::Tokenize(
     if (pos > word_start) {
       absl::string_view word_view(text.data() + word_start, pos - word_start);
 
-      std::string word = absl::AsciiStrToLower(word_view);
+      std::string word = UnicodeNormalizer::CaseFold(word_view);
 
       if (IsStopWord(word)) {
         continue;  // Skip stop words

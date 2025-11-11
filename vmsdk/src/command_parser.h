4b7b07f9d1bb22329692285711784521eb2aa342
Miles Song <bodasong@amazon.com>
2025-11-11 19:53:02 +0000

    fix format
    Signed-off-by: Miles Song <bodasong@amazon.com>

--- vmsdk/src/command_parser.h
+++ vmsdk/src/command_parser.h
@@ -208,7 +208,7 @@ absl::Status ParseEnumParam(
         return absl::OkStatus();                                    \
       })
 
-#define GENERATE_NEGATED_FLAG_PARSER(type, field_name)               \
+#define GENERATE_NEGATED_FLAG_PARSER(type, field_name)              \
   std::make_unique<::vmsdk::ParamParser<type>>(                     \
       [](type &value, ::vmsdk::ArgsIterator &itr) -> absl::Status { \
         value.field_name = false;                                   \

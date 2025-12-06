a4185ce13bdf7dffc4ff8277a016863d71d055cb
Karthik Subbarao <karthikrs2021@gmail.com>
2025-12-06 02:18:09 +0000

    add limit
    Signed-off-by: Karthik Subbarao <karthikrs2021@gmail.com>

--- src/commands/ft_search.cc
+++ src/commands/ft_search.cc
@@ -73,7 +73,8 @@ void SendReplyNoContent(ValkeyModuleCtx *ctx,
                         const std::deque<indexes::Neighbor> &neighbors,
                         const query::SearchParameters &parameters) {
   const size_t start_index = CalcStartIndex(neighbors, parameters);
-  const size_t end_index = start_index + CalcEndIndex(neighbors, parameters);
+  const size_t end_index = std::min(
+      start_index + CalcEndIndex(neighbors, parameters), neighbors.size());
   ValkeyModule_ReplyWithArray(ctx, end_index - start_index + 1);
   ReplyAvailNeighbors(ctx, neighbors, parameters);
   for (auto i = start_index; i < end_index; ++i) {
@@ -95,7 +96,8 @@ void SerializeNeighbors(ValkeyModuleCtx *ctx,
                         const query::SearchParameters &parameters) {
   CHECK_GT(static_cast<size_t>(parameters.k), parameters.limit.first_index);
   const size_t start_index = CalcStartIndex(neighbors, parameters);
-  const size_t end_index = start_index + CalcEndIndex(neighbors, parameters);
+  const size_t end_index = std::min(
+      start_index + CalcEndIndex(neighbors, parameters), neighbors.size());
   ValkeyModule_ReplyWithArray(ctx, 2 * (end_index - start_index) + 1);
   ReplyAvailNeighbors(ctx, neighbors, parameters);
 
@@ -140,7 +142,8 @@ void SerializeNonVectorNeighbors(ValkeyModuleCtx *ctx,
                                  const std::deque<indexes::Neighbor> &neighbors,
                                  const query::SearchParameters &parameters) {
   const size_t start_index = CalcStartIndex(neighbors, parameters);
-  const size_t end_index = start_index + CalcEndIndex(neighbors, parameters);
+  const size_t end_index = std::min(
+      start_index + CalcEndIndex(neighbors, parameters), neighbors.size());
   ValkeyModule_ReplyWithArray(ctx, 2 * (end_index - start_index) + 1);
   ReplyAvailNeighbors(ctx, neighbors, parameters);
   for (size_t i = start_index; i < end_index; ++i) {

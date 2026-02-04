a8865cf51dbed2619eec3ed6499f51996cc5d12e
Manish Addanki <daddaman@amazon.com>
2026-02-04 02:37:33 +0000

    Move query counters to parser file in main thread
    Signed-off-by: Manish Addanki <daddaman@amazon.com>

--- src/query/search.cc
+++ src/query/search.cc
@@ -48,14 +48,6 @@
 #include "vmsdk/src/time_sliced_mrmw_mutex.h"
 #include "vmsdk/src/type_conversions.h"
 #include "vmsdk/src/valkey_module_api/valkey_module.h"
-// Query operation counters
-DEV_INTEGER_COUNTER(query_stats, query_text_term_count);
-DEV_INTEGER_COUNTER(query_stats, query_text_prefix_count);
-DEV_INTEGER_COUNTER(query_stats, query_text_suffix_count);
-DEV_INTEGER_COUNTER(query_stats, query_text_fuzzy_count);
-DEV_INTEGER_COUNTER(query_stats, query_text_proximity_count);
-DEV_INTEGER_COUNTER(query_stats, query_numeric_count);
-DEV_INTEGER_COUNTER(query_stats, query_tag_count);
 
 namespace valkey_search::query {
 
@@ -549,41 +541,6 @@ absl::StatusOr<std::vector<indexes::Neighbor>> SearchNonVectorQuery(
   return neighbors;
 }
 
-// Increment query operation metrics based on query operations flags.
-// Doesn't count fanned out requests
-void IncrementQueryOperationMetrics(QueryOperations query_operations,
-                                    SearchMode search_mode) {
-  if (search_mode != SearchMode::kLocal) {
-    return;
-  }
-  // High-level query type metrics
-  if (query_operations & QueryOperations::kContainsText) {
-    ++Metrics::GetStats().query_text_requests_cnt;
-  }
-  if (query_operations & QueryOperations::kContainsNumeric) {
-    query_numeric_count.Increment();
-  }
-  if (query_operations & QueryOperations::kContainsTag) {
-    query_tag_count.Increment();
-  }
-  // Text operation type metrics
-  if (query_operations & QueryOperations::kContainsTextTerm) {
-    query_text_term_count.Increment();
-  }
-  if (query_operations & QueryOperations::kContainsTextPrefix) {
-    query_text_prefix_count.Increment();
-  }
-  if (query_operations & QueryOperations::kContainsTextSuffix) {
-    query_text_suffix_count.Increment();
-  }
-  if (query_operations & QueryOperations::kContainsTextFuzzy) {
-    query_text_fuzzy_count.Increment();
-  }
-  if (query_operations & QueryOperations::kContainsProximity) {
-    query_text_proximity_count.Increment();
-  }
-}
-
 absl::StatusOr<std::vector<indexes::Neighbor>> DoSearch(
     const SearchParameters &parameters, SearchMode search_mode) {
   // Handle OOM for search requests, defends against request
@@ -599,13 +556,8 @@ absl::StatusOr<std::vector<indexes::Neighbor>> DoSearch(
   auto &time_sliced_mutex = parameters.index_schema->GetTimeSlicedMutex();
   vmsdk::ReaderMutexLock lock(&time_sliced_mutex);
   ++Metrics::GetStats().time_slice_queries;
-  IncrementQueryOperationMetrics(
-      parameters.filter_parse_results.query_operations, search_mode);
   // Handle non vector queries first where attribute_alias is empty.
   if (parameters.IsNonVectorQuery()) {
-    if (search_mode == SearchMode::kLocal) {
-      ++Metrics::GetStats().query_nonvector_requests_cnt;
-    }
     return SearchNonVectorQuery(parameters);
   }
   VMSDK_ASSIGN_OR_RETURN(auto index, parameters.index_schema->GetIndex(
@@ -618,9 +570,6 @@ absl::StatusOr<std::vector<indexes::Neighbor>> DoSearch(
   }
 
   if (!parameters.filter_parse_results.root_predicate) {
-    if (search_mode == SearchMode::kLocal) {
-      ++Metrics::GetStats().query_vector_requests_cnt;
-    }
     return PerformVectorSearch(vector_index, parameters);
   }
   std::queue<std::unique_ptr<indexes::EntriesFetcherBase>> entries_fetchers;

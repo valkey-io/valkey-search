6c61ccb06921e7d8beb2397d2547a5a4ffaffba5
KarthikSubbarao <karthikrs2021@gmail.com>
2026-03-02 13:05:04 -0600

    Fix crash due to lazy expiration in the middle of Search commands (#831)
    Fix crash due to lazy expiration in the middle of Search commands.
    
    I looked into the numops crash for some time this evening. I was able to
    reproduce it from an integration test with HSET, FT.SEARCH and EXPIRE
    (no other operations) commands being executed in parallel. The setup
    needed a primary and a replica.
    
    From what I can tell, the crash only happens when there is an expiration
    during the workload that would lead to the core trying to propagate the
    UNLINK command to the replica node. Expiration happens on the primary
    and the command is sent as UNLINK.
    
    When using a very high TTL in the EXPIRE command, this crash is not
    encountered.
    Primary queues UNLINK command for replication to replica, but
    also_propagate queue isn't flushed before handleClientsBlockedOnKeys()
    is called (from beforeSleep) and this leads to the assert.
    
    Understanding the state in the blocked.c will help figure out the next
    steps
    
    Note: From my tests, at the time of the crash, I have seen at least one
    client with the blocked flag. e.g.:
    ```
    id=332 addr=127.0.0.1:57310 laddr=127.0.0.1:19311 fd=187 name=*redacted* age=3 idle=0 flags=b capa= db=0 sub=0 psub=0 ssub=0 multi=-1 watch=0 qbuf=0 qbuf-free=0 argv-mem=28 multi-mem=0 rbs=1024 rbp=570 obl=0 oll=0 omem=0 tot-mem=1716 events=r cmd=FT.SEARCH user=*redacted* redir=-1 resp=2 lib-name=valkey-py lib-ver=6.1.1 tot-net-in=8090 tot-net-out=89416 tot-cmds=165
    ```
    
    The crash was happening because there was lazy expiration in the middle
    of Search based commands.
    
    The Search command would access the key and mark it for lazy expiration
    (this is the default mode). This is due to how the Module was opening
    the key when fetching content using `ValkeyModule_OpenKey`.
    
    The key would be added to the also_propagate queue in the core and when
    we are done with the current command (example: Search), it would crash
    on the num_ops assert in handleClientsBlockedOnKeys called from
    blockedBeforeSleep because it already has the UNLINK command from
    expiration.
    
    I updated how the module opens the key to include flags (e.g.
    `VALKEYMODULE_OPEN_KEY_NOEXPIRE` or simply
    `VALKEYMODULE_OPEN_KEY_NOEFFECTS`) to avoid having the searched keys
    marked for lazy expiration. This prevents the crash:
    https://github.com/valkey-io/valkey-search/pull/831
    
    In all other sites in the Module we were using
    `VALKEYMODULE_OPEN_KEY_NOEFFECTS | VALKEYMODULE_READ`, except for the
    `FetchAllRecords` function.
    
    If we use this `VALKEYMODULE_OPEN_KEY_NOEFFECTS `, we do not encounter
    the crash. But also, we miss out on the following:
    ```
    VALKEYMODULE_OPEN_KEY_NOTOUCH - Avoid touching the LRU/LFU of the key when opened.
    VALKEYMODULE_OPEN_KEY_NONOTIFY - Don't trigger keyspace event on key misses.
    VALKEYMODULE_OPEN_KEY_NOSTATS - Don't update keyspace hits/misses counters.
    VALKEYMODULE_OPEN_KEY_NOEXPIRE - Avoid deleting lazy expired keys.
    VALKEYMODULE_OPEN_KEY_NOEFFECTS - Avoid any effects from fetching the key.
    ```
    
    So, instead, if we use `VALKEYMODULE_OPEN_KEY_NOEXPIRE`, we will get all
    other capabilities and avoid crash due to lazy expire. It will have to
    be expired by other core operations.
    
    I also updated the PR to perform an expiration check
    (ValkeyModule_GetExpire) and we skip opening the key in the FetchRecords
    fn
    
    ---------
    
    Signed-off-by: KarthikSubbarao <karthikrs2021@gmail.com>
    Backport-Source-PR: 831

--- testing/query/response_generator_test.cc
+++ testing/query/response_generator_test.cc
@@ -77,6 +77,9 @@ TEST_P(ResponseGeneratorTest, ProcessNeighborsForReply) {
   auto &params = GetParam();
   ValkeyModuleCtx fake_ctx;
 
+  EXPECT_CALL(*kMockValkeyModule, GetExpire(testing::_))
+      .WillRepeatedly(testing::Return(VALKEYMODULE_NO_EXPIRE));
+
   std::deque<indexes::Neighbor> expected_neighbors;
   for (const auto &external_id : params.external_id_neighbors) {
     auto string_interned_external_id = StringInternStore::Intern(external_id);
@@ -153,6 +156,9 @@ TEST_P(ResponseGeneratorTest, ProcessNeighborsForReply) {
 
 TEST_F(ResponseGeneratorTest, ProcessNeighborsForReplyContentLimits) {
   ValkeyModuleCtx fake_ctx;
+
+  EXPECT_CALL(*kMockValkeyModule, GetExpire(testing::_))
+      .WillRepeatedly(testing::Return(VALKEYMODULE_NO_EXPIRE));
 
   // Set up a small content size limit for testing
   const size_t test_size_limit = 100;
@@ -550,6 +556,8 @@ TEST_P(ResponseGeneratorDbParamTest, ProcessNeighborsForReplySelectsCorrectDB) {
   int target_db = 5;
   int original_db = 0;
   data_model::AttributeDataType type = GetParam();
+  EXPECT_CALL(*kMockValkeyModule, GetExpire(testing::_))
+      .WillRepeatedly(testing::Return(VALKEYMODULE_NO_EXPIRE));
 
   query::SearchParameters parameters(100000, nullptr, 0);
   parameters.db_num = target_db;
@@ -594,6 +602,9 @@ TEST_P(ResponseGeneratorDbParamTest, ProcessNeighborsForReplyNoContent) {
   int target_db = 5;
   int original_db = 0;
   data_model::AttributeDataType type = GetParam();
+
+  EXPECT_CALL(*kMockValkeyModule, GetExpire(testing::_))
+      .WillRepeatedly(testing::Return(VALKEYMODULE_NO_EXPIRE));
 
   query::SearchParameters parameters(100000, nullptr, 0);
   parameters.db_num = target_db;

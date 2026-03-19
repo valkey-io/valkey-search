7ba10a24aa8ed9aa721b58f6aeed37c44ffe1e6a
Riley Des <riley.desserre@improving.com>
2026-03-19 15:36:56 -0500

    fix: always replicate alias commands regardless of coordinator mode
    Signed-off-by: Riley Des <riley.desserre@improving.com>

--- src/commands/ft_alias_add.cc
+++ src/commands/ft_alias_add.cc
@@ -30,9 +30,8 @@ absl::Status FTAliasAddCmd(ValkeyModuleCtx *ctx, ValkeyModuleString **argv,
 
   ValkeyModule_ReplyWithSimpleString(ctx, "OK");
 
-  if (!options::GetUseCoordinator().GetValue()) {
-    ValkeyModule_ReplicateVerbatim(ctx);
-  }
+  // Unlike indexes, aliases have no MetadataManager path, therefore always replicate.
+  ValkeyModule_ReplicateVerbatim(ctx);
   return absl::OkStatus();
 }
 

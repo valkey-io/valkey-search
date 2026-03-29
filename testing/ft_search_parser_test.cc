90224e268c887fcb3fedee7c3d060de70848382b
Su Ko <rhtn1128@gmail.com>
2026-03-29 22:46:02 +0900

    Fix numeric range search doesn't accept scientific notation
    Signed-off-by: Su Ko <rhtn1128@gmail.com>

--- testing/ft_search_parser_test.cc
+++ testing/ft_search_parser_test.cc
@@ -437,6 +437,17 @@ INSTANTIATE_TEST_SUITE_P(
             .vector_query = false,
         },
         {
+            .test_name = "happy_path_numeric_scientific_notation",
+            .success = true,
+            .params_str = "",
+            .filter_str = "@attribute_identifier_1:[1.0 1e3]",
+            .attribute_alias = "",
+            .k = 0,
+            .ef = 0,
+            .score_as = "",
+            .vector_query = false,
+        },
+        {
             .test_name = "happy_path_tag",
             .success = true,
             .params_str = "",

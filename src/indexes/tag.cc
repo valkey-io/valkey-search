7fa83db3266b3ea1fa2235cf0f4c02cc0562ad0e
Su Ko <rhtn1128@gmail.com>
2026-04-05 15:39:58 +0900

    Fix normalize wildcard string-array values for TAG indexing
    Signed-off-by: Su Ko <rhtn1128@gmail.com>

--- src/indexes/tag.cc
+++ src/indexes/tag.cc
@@ -18,6 +18,7 @@
 #include "absl/status/statusor.h"
 #include "absl/strings/ascii.h"
 #include "absl/strings/str_cat.h"
+#include "absl/strings/str_replace.h"
 #include "absl/strings/str_split.h"
 #include "absl/strings/string_view.h"
 #include "absl/synchronization/mutex.h"
@@ -257,6 +258,21 @@ absl::StatusOr<RecordResult> Tag::ModifyRecord(const InternedStringPtr &key,
 
   tag_info.raw_tag_string = std::move(interned_data);
   return RecordResult::kAdded;
+}
+
+vmsdk::UniqueValkeyString Tag::NormalizeStringAttribute(
+    vmsdk::UniqueValkeyString input) const {
+  if (!input) {
+    return input;
+  }
+  auto record = vmsdk::ToStringView(input.get());
+  if (record.find("\",\"") == absl::string_view::npos) {
+    return input;
+  }
+
+  std::string replacement(1, separator_);
+  return vmsdk::MakeUniqueValkeyString(
+      absl::StrReplaceAll(record, {{"\",\"", replacement}}));
 }
 
 absl::StatusOr<bool> Tag::RemoveRecord(const InternedStringPtr &key,

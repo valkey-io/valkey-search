dbb25709f8c56b644173b536dab8a51626dcfd16
Aksha Thakkar <thaakb@amazon.com>
2026-05-20 02:38:37 -0700

    Cherry-pick: Fix tag OR syntax in hybrid queries and add filter expression tests (#452) to 1.0
    Backport of PR #452 to 1.0 branch.
    
    Conflict resolution: adapted TagPredicate constructor to 1.0 signature
    (no alias parameter, uses MakeUniqueRedisString).
    
    Signed-off-by: Aksha1812 <thaakb@amazon.com>
    Signed-off-by: Aksha Thakkar <thaakb@amazon.com>

--- src/query/predicate.cc
+++ src/query/predicate.cc
@@ -79,8 +79,12 @@ TagPredicate::TagPredicate(const indexes::Tag* index,
     : Predicate(PredicateType::kTag),
       index_(index),
       identifier_(vmsdk::MakeUniqueRedisString(identifier)),
-      raw_tag_string_(raw_tag_string),
-      tags_(tags.begin(), tags.end()) {}
+      raw_tag_string_(raw_tag_string) {
+  // Unescape each tag (e.g., \| -> |, \\ -> \)
+  for (const auto& tag : tags) {
+    tags_.insert(indexes::Tag::UnescapeTag(tag));
+  }
+}
 
 bool TagPredicate::Evaluate(Evaluator& evaluator) const {
   return evaluator.EvaluateTags(*this);

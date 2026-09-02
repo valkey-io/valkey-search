zvi-code/z-valkey-search

commit 2dc8e1f6a273f945e54828731c59a8db68983481

Add SCORE_FIELD support and SCORE range validation to FT.CREATE (#1008)
## Summary

Adds support for the `SCORE_FIELD` parameter in `FT.CREATE`, changes
`SCORE` validation from accepting only `1.0` to accepting any value in
`[0.0, 1.0]`, and reads per-document score field values during
ingestion.

---------

Signed-off-by: Shaopeng Gu <gushaopengfrank@gmail.com>

src/index_schema.cc
@@ -9,6 +9,7 @@
 
 #include <algorithm>
 #include <atomic>
+#include <cmath>
 #include <cstddef>
 #include <cstdint>
 #include <cstring>
@@ -24,6 +25,7 @@
 #include "absl/log/check.h"
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
+#include "absl/strings/numbers.h"
 #include "absl/strings/str_cat.h"
 #include "absl/strings/str_format.h"
 #include "absl/strings/string_view.h"
@@ -298,6 +300,11 @@ IndexSchema::IndexSchema(ValkeyModuleCtx *ctx,
       min_stem_size_(index_schema_proto.min_stem_size() > 0
                          ? index_schema_proto.min_stem_size()
                          : 4),
+      score_(index_schema_proto.has_score() ? index_schema_proto.score()
+                                            : kDefaultDocumentScore),
+      score_field_(index_schema_proto.has_score_field()
+                       ? std::make_optional(index_schema_proto.score_field())
+                       : std::nullopt),
       mutations_thread_pool_(mutations_thread_pool),
       time_sliced_mutex_(CreateMrmwMutexOptions()) {
   ValkeyModule_SelectDb(detached_ctx_.get(), db_num_);
@@ -586,6 +593,24 @@ void IndexSchema::ProcessKeyspaceNotification(ValkeyModuleCtx *ctx,
       added = true;
     }
   }
+
+  // Read the per-document score from SCORE_FIELD if configured
+  float document_score = score_;
+  if (key_obj && HasScoreField()) {
+    auto score_record = attribute_data_type_->GetRecord(
+        ctx, key_obj.get(), key_cstr, score_field_.value());
+    if (score_record.ok()) {
+      // Parse the value as a float. If it fails (non-numeric), fall back to
+      // the default score silently. Raw value is stored without clamping.
+      // The scoring algorithm decides how to handle values at query time.
+      float value;
+      auto str_view = vmsdk::ToStringView(score_record.value().get());
+      if (absl::SimpleAtof(str_view, &value) && value == value) {  // NaN check
+        document_score = value;
+      }
+    }
+  }
+
   if (added) {
     switch (attribute_data_type_->ToProto()) {
       case data_model::ATTRIBUTE_DATA_TYPE_HASH:
@@ -606,7 +631,7 @@ void IndexSchema::ProcessKeyspaceNotification(ValkeyModuleCtx *ctx,
         CHECK(false);
     }
     ProcessMutation(ctx, mutated_attributes, interned_key, from_backfill,
-                    key_obj == nullptr);
+                    key_obj == nullptr, document_score);
   }
 }
 
@@ -864,13 +889,14 @@ MutationSequenceNumber IndexSchema::UpdateDbInfoKey(
 void IndexSchema::ProcessMutation(ValkeyModuleCtx *ctx,
                                   MutatedAttributes &mutated_attributes,
                                   const Key &interned_key, bool from_backfill,
-                                  bool is_delete) {
+                                  bool is_delete, float document_score) {
   auto this_mutation = UpdateDbInfoKey(ctx, mutated_attributes, interned_key,
                                        from_backfill, is_delete);
 
   if (ABSL_PREDICT_FALSE(!mutations_thread_pool_ ||
                          mutations_thread_pool_->Size() == 0)) {
     vmsdk::WriterMutexLock lock(&time_sliced_mutex_);
+    index_key_info_[interned_key].document_score = document_score;
     SyncProcessMutation(ctx, mutated_attributes, interned_key);
     return;
   }
@@ -883,7 +909,7 @@ void IndexSchema::ProcessMutation(ValkeyModuleCtx *ctx,
 
   if (ABSL_PREDICT_FALSE(!TrackMutatedRecord(
           ctx, interned_key, std::move(mutated_attributes), this_mutation,
-          from_backfill, block_client, inside_multi_exec)) ||
+          from_backfill, block_client, inside_multi_exec, document_score)) ||
       inside_multi_exec) {
     // Skip scheduling if the mutation key has already been tracked or is part
     // of a multi exec command.
@@ -1097,7 +1123,7 @@ void IndexSchema::RespondWithInfo(ValkeyModuleCtx *ctx) const {
   ValkeyModule_ReplyWithSimpleString(ctx, name_.data());
 
   ValkeyModule_ReplyWithSimpleString(ctx, "index_definition");
-  ValkeyModule_ReplyWithArray(ctx, 6);
+  ValkeyModule_ReplyWithArray(ctx, 8);
   ValkeyModule_ReplyWithSimpleString(ctx, "key_type");
   ValkeyModule_ReplyWithSimpleString(ctx,
                                      attribute_data_type_->ToString().c_str());
@@ -1106,10 +1132,12 @@ void IndexSchema::RespondWithInfo(ValkeyModuleCtx *ctx) const {
   for (const auto &prefix : subscribed_key_prefixes_) {
     ValkeyModule_ReplyWithSimpleString(ctx, prefix.c_str());
   }
-  // hard-code default score of 1 as it's the only value we currently
-  // supported.
   ValkeyModule_ReplyWithSimpleString(ctx, "default_score");
-  ValkeyModule_ReplyWithCString(ctx, "1");
+  ValkeyModule_ReplyWithDouble(ctx, static_cast<double>(score_));
+
+  ValkeyModule_ReplyWithSimpleString(ctx, "score_field");
+  ValkeyModule_ReplyWithSimpleString(
+      ctx, score_field_.has_value() ? score_field_.value().c_str() : "");
 
   ValkeyModule_ReplyWithSimpleString(ctx, "attributes");
   ValkeyModule_ReplyWithArray(ctx, VALKEYMODULE_POSTPONED_ARRAY_LEN);
@@ -1209,6 +1237,10 @@ std::unique_ptr<data_model::IndexSchema> IndexSchema::ToProto() const {
   index_schema_proto->mutable_stop_words()->Assign(stop_words_.begin(),
                                                    stop_words_.end());
   index_schema_proto->set_skip_initial_scan(skip_initial_scan_);
+  index_schema_proto->set_score(score_);
+  if (score_field_.has_value()) {
+    index_schema_proto->set_score_field(score_field_.value());
+  }
 
   auto stats = index_schema_proto->mutable_stats();
   stats->set_documents_count(stats_.document_cnt);
@@ -1864,7 +1896,7 @@ bool IndexSchema::TrackMutatedRecord(ValkeyModuleCtx *ctx, const Key &key,
                                      MutatedAttributes &&mutated_attributes,
                                      MutationSequenceNumber sequence_number,
                                      bool from_backfill, bool block_client,
-                                     bool from_multi) {
+                                     bool from_multi, float document_score) {
   absl::MutexLock lock(&mutated_records_mutex_);
   auto [itr, inserted] =
       tracked_mutated_records_.insert({key, DocumentMutation{}});
@@ -1874,6 +1906,7 @@ bool IndexSchema::TrackMutatedRecord(ValkeyModuleCtx *ctx, const Key &key,
     itr->second.from_backfill = from_backfill;
     itr->second.from_multi = from_multi;
     itr->second.sequence_number = sequence_number;
+    itr->second.document_score = document_score;
     // Allocate memory buffer proportional to data size and mutation weights
     // Buffer is freed when the mutation record is erased.
     itr->second.weighted_buffer.resize(
@@ -1888,6 +1921,7 @@ bool IndexSchema::TrackMutatedRecord(ValkeyModuleCtx *ctx, const Key &key,
   }
 
   itr->second.sequence_number = sequence_number;
+  itr->second.document_score = document_score;
 
   if (!itr->second.from_multi && from_multi) {
     itr->second.from_multi = from_multi;
@@ -1965,8 +1999,9 @@ IndexSchema::ConsumeTrackedMutatedAttribute(const Key &key, bool first_time) {
       tracked_mutated_records_.erase(itr);
       // Will notify after releasing lock
     } else {
-      index_key_info_[key].mutation_sequence_number_ =
-          itr->second.sequence_number;
+      auto &key_info = index_key_info_[key];
+      key_info.mutation_sequence_number_ = itr->second.sequence_number;
+      key_info.document_score = itr->second.document_score;
       // Track entry is now first consumed
       auto mutated_attributes = std::move(itr->second.attributes.value());
       itr->second.attributes = std::nullopt;

testing/ft_info_test.cc
@@ -49,10 +49,10 @@ struct MultiFtInfoTestCase {
 class FTInfoTest : public ValkeySearchTestWithParam<MultiFtInfoTestCase> {};
 
 TEST_P(FTInfoTest, FTInfoTests) {
-  const MultiFtInfoTestCase& test_cases = GetParam();
+  const MultiFtInfoTestCase &test_cases = GetParam();
 
   for (bool use_thread_pool : {true, false}) {
-    for (const auto& test_case : test_cases.test_cases) {
+    for (const auto &test_case : test_cases.test_cases) {
       fake_ctx_ = ValkeyModuleCtx{};
       // Set up the data structures for the test case.
       vmsdk::ThreadPool mutations_thread_pool("writer-thread-pool-", 5);
@@ -74,7 +74,7 @@ TEST_P(FTInfoTest, FTInfoTests) {
       }
 
       // Run the command.
-      std::vector<ValkeyModuleString*> cmd_argv;
+      std::vector<ValkeyModuleString *> cmd_argv;
       std::transform(test_case.argv.begin(), test_case.argv.end(),
                      std::back_inserter(cmd_argv), [&](std::string val) {
                        return TestValkeyModule_CreateStringPrintf(
@@ -135,9 +135,10 @@ INSTANTIATE_TEST_SUITE_P(
                         .expect_return_failure = false,
                         .expected_output =
                             "*28\r\n+index_name\r\n+test_name\r\n+index_"
-                            "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                            "1\r\n1\r\n+attributes\r\n*1\r\n*10\r\n+"
+                            "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                            "1\r\n+score_field\r\n+\r\n+"
+                            "attributes\r\n*1\r\n*10\r\n+"
                             "identifier\r\n+test_identifier_1\r\n+"
                             "attribute\r\n+test_attribute_1\r\n+user_indexed_"
                             "memory\r\n:0\r\n+type\r\n+VECTOR\r\n+index\r\n*"
@@ -191,9 +192,10 @@ INSTANTIATE_TEST_SUITE_P(
                         .expect_return_failure = false,
                         .expected_output =
                             "*28\r\n+index_name\r\n+test_name\r\n+index_"
-                            "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                            "1\r\n1\r\n+attributes\r\n*1\r\n*10\r\n+"
+                            "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                            "1\r\n+score_field\r\n+\r\n+"
+                            "attributes\r\n*1\r\n*10\r\n+"
                             "identifier\r\n+test_identifier_1\r\n+"
                             "attribute\r\n+test_attribute_1\r\n+user_indexed_"
                             "memory\r\n:0\r\n+type\r\n+VECTOR\r\n+index\r\n*"
@@ -239,9 +241,10 @@ INSTANTIATE_TEST_SUITE_P(
                         .expect_return_failure = false,
                         .expected_output =
                             "*28\r\n+index_name\r\n+test_name\r\n+index_"
-                            "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                            "1\r\n1\r\n+attributes\r\n*1\r\n*14\r\n+"
+                            "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                            "1\r\n+score_field\r\n+\r\n+"
+                            "attributes\r\n*1\r\n*14\r\n+"
                             "identifier\r\n+test_identifier_1\r\n+"
                             "attribute\r\n+test_attribute_1\r\n+user_indexed_"
                             "memory\r\n:0\r\n+type\r\n+TAG\r\n+SEPARATOR\r\n+@"
@@ -283,9 +286,10 @@ INSTANTIATE_TEST_SUITE_P(
                         .expect_return_failure = false,
                         .expected_output =
                             "*28\r\n+index_name\r\n+test_name\r\n+index_"
-                            "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                            "1\r\n1\r\n+attributes\r\n*1\r\n*14\r\n+"
+                            "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                            "1\r\n+score_field\r\n+\r\n+"
+                            "attributes\r\n*1\r\n*14\r\n+"
                             "identifier\r\n+test_identifier_1\r\n+"
                             "attribute\r\n+test_attribute_1\r\n+user_indexed_"
                             "memory\r\n:0\r\n+type\r\n+TAG\r\n+SEPARATOR\r\n+@"
@@ -324,9 +328,10 @@ INSTANTIATE_TEST_SUITE_P(
                         .expect_return_failure = false,
                         .expected_output =
                             "*28\r\n+index_name\r\n+test_name\r\n+index_"
-                            "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                            "1\r\n1\r\n+attributes\r\n*1\r\n*10\r\n+"
+                            "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                            "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                            "1\r\n+score_field\r\n+\r\n+"
+                            "attributes\r\n*1\r\n*10\r\n+"
                             "identifier\r\n+test_identifier_1\r\n+"
                             "attribute\r\n+test_attribute_1\r\n+user_indexed_"
                             "memory\r\n:0\r\n+type\r\n+NUMERIC\r\n+size\r\n$"
@@ -393,9 +398,10 @@ INSTANTIATE_TEST_SUITE_P(
                      .expect_return_failure = false,
                      .expected_output =
                          "*36\r\n+index_name\r\n+test_name\r\n+index_"
-                         "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                         "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                         "1\r\n1\r\n+attributes\r\n*1\r\n*14\r\n+"
+                         "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                         "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                         "1\r\n+score_field\r\n+\r\n+attributes\r\n*"
+                         "1\r\n*14\r\n+"
                          "identifier\r\n+test_identifier_1\r\n+attribute\r\n+"
                          "test_attribute_1\r\n+user_indexed_memory\r\n:0\r\n+"
                          "type\r\n+TEXT\r\n+WITH_SUFFIX_TRIE\r\n+0\r\n+NO_"
@@ -445,9 +451,10 @@ INSTANTIATE_TEST_SUITE_P(
                      .expect_return_failure = false,
                      .expected_output =
                          "*36\r\n+index_name\r\n+test_name\r\n+index_"
-                         "definition\r\n*6\r\n+key_type\r\n+HASH\r\n+"
-                         "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n$"
-                         "1\r\n1\r\n+attributes\r\n*1\r\n*14\r\n+"
+                         "definition\r\n*8\r\n+key_type\r\n+HASH\r\n+"
+                         "prefixes\r\n*1\r\n+prefix_1\r\n+default_score\r\n"
+                         "1\r\n+score_field\r\n+\r\n+attributes\r\n*"
+                         "1\r\n*14\r\n+"
                          "identifier\r\n+test_identifier_1\r\n+attribute\r\n+"
                          "test_attribute_1\r\n+user_indexed_memory\r\n:0\r\n+"
                          "type\r\n+TEXT\r\n+WITH_SUFFIX_TRIE\r\n+1\r\n+NO_"
@@ -502,7 +509,7 @@ INSTANTIATE_TEST_SUITE_P(
                 },
         },
     }),
-    [](const TestParamInfo<MultiFtInfoTestCase>& info) {
+    [](const TestParamInfo<MultiFtInfoTestCase> &info) {
       return info.param.test_name;
     });
 

src/commands/ft_create_parser.cc
@@ -24,6 +24,7 @@
 #include "absl/strings/str_cat.h"
 #include "absl/strings/string_view.h"
 #include "src/attribute_data_type.h"
+#include "src/index_schema.h"
 #include "src/index_schema.pb.h"
 #include "src/indexes/index_base.h"
 #include "src/indexes/vector_base.h"
@@ -284,21 +285,26 @@ absl::Status ParseLanguage(vmsdk::ArgsIterator &itr,
 }
 absl::Status ParseScore(vmsdk::ArgsIterator &itr,
                         data_model::IndexSchema &index_schema_proto) {
-  float score{1.0};
+  float score{IndexSchema::kDefaultDocumentScore};
   VMSDK_ASSIGN_OR_RETURN(auto res,
                          vmsdk::ParseParam(kScoreParam, false, itr, score));
-  if (res && score != 1.0) {
-    return absl::InvalidArgumentError(
-        absl::StrCat("`", kScoreParam, "` parameter with a value `", score,
-                     "` is not supported. The only supported value is `1.0`"));
-  }
-  VMSDK_ASSIGN_OR_RETURN(res,
-                         vmsdk::IsParamKeyMatch(kScoreFieldParam, false, itr));
   if (res) {
-    return absl::InvalidArgumentError(
-        NotSupportedParamErrorMsg(kScoreFieldParam));
+    if (score < 0.0 || score > 1.0) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("`", kScoreParam, "` parameter with a value `", score,
+                       "` is not supported. The value must be between "
+                       "0.0 and 1.0"));
+    }
+    index_schema_proto.set_score(score);
   }
-  index_schema_proto.set_score(score);
+  // Parse SCORE_FIELD: the name of the document field whose numeric value
+  // is used as the document's custom score. Only one per index.
+  std::string score_field;
+  VMSDK_ASSIGN_OR_RETURN(
+      res, vmsdk::ParseParam(kScoreFieldParam, false, itr, score_field));
+  if (res) {
+    index_schema_proto.set_score_field(score_field);
+  }
   return absl::OkStatus();
 }
 vmsdk::KeyValueParser<HNSWParameters> CreateHNSWParser() {

valkey-io/valkey-search

commit 2fb0ad771fdea05b9ea6c74d71cf51672f801bbf

fix breaking changes introduced in #1008
Signed-off-by: Shaopeng Gu <shpenggu@amazon.com>

src/index_schema.cc
@@ -1203,6 +1203,18 @@ IndexSchema::GetSortedAttributes() const {
 }
 
 void IndexSchema::RespondWithInfo(ValkeyModuleCtx *ctx) const {
+  // The index_definition block gained the score_field pair and switched
+  // default_score from a hardcoded "1" bulk string to the configured score as
+  // a double in 1.3.0. Pre-1.3.0: a 6-element array with no score_field and a
+  // literal "1" for default_score (SCORE only accepted 1.0 at the time, so the
+  // literal was always accurate). Evaluated once per reply so the element count
+  // and the emitted pairs cannot disagree, and so the INFO counter records one
+  // use per FT.INFO call rather than one per affected field. See
+  // COMPATIBILITY.md.
+  const bool score_info_fixed = VALKEY_SEARCH_COMPATIBILITY_FIX(
+      1, 3, 0, "ft_info_score_field", [] { return true; },
+      [] { return false; });
+
   int arrSize = 28;
   // Text-attribute info fields
   if (text_index_schema_) {
@@ -1214,7 +1226,7 @@ void IndexSchema::RespondWithInfo(ValkeyModuleCtx *ctx) const {
   ValkeyModule_ReplyWithSimpleString(ctx, name_.data());
 
   ValkeyModule_ReplyWithSimpleString(ctx, "index_definition");
-  ValkeyModule_ReplyWithArray(ctx, 8);
+  ValkeyModule_ReplyWithArray(ctx, score_info_fixed ? 8 : 6);
   ValkeyModule_ReplyWithSimpleString(ctx, "key_type");
   ValkeyModule_ReplyWithSimpleString(ctx,
                                      attribute_data_type_->ToString().c_str());
@@ -1224,11 +1236,15 @@ void IndexSchema::RespondWithInfo(ValkeyModuleCtx *ctx) const {
     ValkeyModule_ReplyWithSimpleString(ctx, prefix.c_str());
   }
   ValkeyModule_ReplyWithSimpleString(ctx, "default_score");
-  ValkeyModule_ReplyWithDouble(ctx, static_cast<double>(score_));
+  if (score_info_fixed) {
+    ValkeyModule_ReplyWithDouble(ctx, static_cast<double>(score_));
 
-  ValkeyModule_ReplyWithSimpleString(ctx, "score_field");
-  ValkeyModule_ReplyWithSimpleString(
-      ctx, score_field_.has_value() ? score_field_.value().c_str() : "");
+    ValkeyModule_ReplyWithSimpleString(ctx, "score_field");
+    ValkeyModule_ReplyWithSimpleString(
+        ctx, score_field_.has_value() ? score_field_.value().c_str() : "");
+  } else {
+    ValkeyModule_ReplyWithCString(ctx, "1");
+  }
 
   ValkeyModule_ReplyWithSimpleString(ctx, "attributes");
   ValkeyModule_ReplyWithArray(ctx, VALKEYMODULE_POSTPONED_ARRAY_LEN);

testing/ft_info_test.cc
@@ -12,6 +12,8 @@
 #include <string>
 #include <vector>
 
+#include "absl/strings/match.h"
+#include "absl/strings/str_replace.h"
 #include "absl/strings/string_view.h"
 #include "gmock/gmock.h"
 #include "google/protobuf/text_format.h"
@@ -19,6 +21,7 @@
 #include "src/commands/commands.h"
 #include "src/index_schema.pb.h"
 #include "src/schema_manager.h"
+#include "src/valkey_search_options.h"
 #include "testing/common.h"
 #include "vmsdk/src/module.h"
 #include "vmsdk/src/testing_infra/module.h"
@@ -46,54 +49,112 @@ struct MultiFtInfoTestCase {
   std::vector<SingleFtInfoTestCase> test_cases;
 };
 
-class FTInfoTest : public ValkeySearchTestWithParam<MultiFtInfoTestCase> {};
+// The FT.INFO index_definition block is gated on search.emulate-release: 1.3.0
+// and later emit an 8-element block whose default_score is a double and which
+// carries a score_field pair, while earlier releases emit a 6-element block
+// with default_score as the bulk string "1" and no score_field. See
+// COMPATIBILITY.md ("compatibility-ft_info_score_field").
+//
+// Each expected_output below is written in the 1.3.0 (fixed) shape; the legacy
+// expectation is derived from it so the two cannot drift apart. The segment is
+// identical across every case, so a single substitution covers all of them.
+constexpr absl::string_view kFixedScoreInfoSegment =
+    "+default_score\r\n1\r\n+score_field\r\n+\r\n";
+constexpr absl::string_view kLegacyScoreInfoSegment =
+    "+default_score\r\n$1\r\n1\r\n";
+
+// Rewrites a fixed-shape expectation into the pre-1.3.0 shape. Replies that do
+// not contain an index_definition block (error cases) are returned unchanged.
+std::string ToLegacyScoreInfoShape(absl::string_view fixed) {
+  if (!absl::StrContains(fixed, kFixedScoreInfoSegment)) {
+    return std::string(fixed);
+  }
+  return absl::StrReplaceAll(
+      fixed, {{"+index_definition\r\n*8\r\n", "+index_definition\r\n*6\r\n"},
+              {kFixedScoreInfoSegment, kLegacyScoreInfoSegment}});
+}
+
+class FTInfoTest : public ValkeySearchTestWithParam<MultiFtInfoTestCase> {
+ protected:
+  void SetUp() override {
+    ValkeySearchTestWithParam<MultiFtInfoTestCase>::SetUp();
+    saved_emulate_release_ = options::GetEmulateRelease().GetValue();
+  }
+  void TearDown() override {
+    VMSDK_EXPECT_OK(
+        options::GetEmulateRelease().SetValue(saved_emulate_release_));
+    ValkeySearchTestWithParam<MultiFtInfoTestCase>::TearDown();
+  }
+
+  // Pinning to 1.3.0 exceeds kModuleVersion, which ValidateEmulateRelease only
+  // permits while debug mode is on. Debug mode defaults to on under the unit
+  // tests (ParseAndLoadArgv, which would clear it, never runs here).
+  static void SetEmulateRelease(vmsdk::ValkeyVersion v) {
+    VMSDK_EXPECT_OK(options::GetEmulateRelease().SetValue(v));
+  }
+
+ private:
+  vmsdk::ValkeyVersion saved_emulate_release_{0};
+};
 
 TEST_P(FTInfoTest, FTInfoTests) {
   const MultiFtInfoTestCase &test_cases = GetParam();
 
-  for (bool use_thread_pool : {true, false}) {
-    for (const auto &test_case : test_cases.test_cases) {
-      fake_ctx_ = ValkeyModuleCtx{};
-      // Set up the data structures for the test case.
-      vmsdk::ThreadPool mutations_thread_pool("writer-thread-pool-", 5);
-      SchemaManager::InitInstance(std::make_unique<TestableSchemaManager>(
-          &fake_ctx_, []() {},
-          use_thread_pool ? &mutations_thread_pool : nullptr, false));
-      data_model::IndexSchema index_schema_proto;
-      if (test_case.index_schema_pbtxt.has_value()) {
-        ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
-            test_case.index_schema_pbtxt.value(), &index_schema_proto));
-        VMSDK_EXPECT_OK(SchemaManager::Instance().CreateIndexSchema(
-            &fake_ctx_, index_schema_proto));
-        EXPECT_EQ(SchemaManager::Instance().GetNumberOfIndexSchemas(), 1);
-      }
+  // Exercise both sides of the compatibility gate. The fixed shape is what the
+  // expectations below are written in; the legacy shape is what ships by
+  // default, since emulate-release defaults to <major>.0.0.
+  for (bool score_info_fixed : {true, false}) {
+    SetEmulateRelease(score_info_fixed ? vmsdk::ValkeyVersion(1, 3, 0)
+                                       : vmsdk::ValkeyVersion(1, 2, 1));
+    SCOPED_TRACE(score_info_fixed ? "emulate-release=1.3.0 (fixed shape)"
+                                  : "emulate-release=1.2.1 (legacy shape)");
+    for (bool use_thread_pool : {true, false}) {
+      for (const auto &test_case : test_cases.test_cases) {
+        fake_ctx_ = ValkeyModuleCtx{};
+        // Set up the data structures for the test case.
+        vmsdk::ThreadPool mutations_thread_pool("writer-thread-pool-", 5);
+        SchemaManager::InitInstance(std::make_unique<TestableSchemaManager>(
+            &fake_ctx_, []() {},
+            use_thread_pool ? &mutations_thread_pool : nullptr, false));
+        data_model::IndexSchema index_schema_proto;
+        if (test_case.index_schema_pbtxt.has_value()) {
+          ASSERT_TRUE(google::protobuf::TextFormat::ParseFromString(
+              test_case.index_schema_pbtxt.value(), &index_schema_proto));
+          VMSDK_EXPECT_OK(SchemaManager::Instance().CreateIndexSchema(
+              &fake_ctx_, index_schema_proto));
+          EXPECT_EQ(SchemaManager::Instance().GetNumberOfIndexSchemas(), 1);
+        }
 
-      if (test_case.expect_return_failure) {
-        EXPECT_CALL(*kMockValkeyModule, ReplyWithError(&fake_ctx_, _))
-            .WillOnce(Return(VALKEYMODULE_OK));
-      }
+        if (test_case.expect_return_failure) {
+          EXPECT_CALL(*kMockValkeyModule, ReplyWithError(&fake_ctx_, _))
+              .WillOnce(Return(VALKEYMODULE_OK));
+        }
 
-      // Run the command.
-      std::vector<ValkeyModuleString *> cmd_argv;
-      std::transform(test_case.argv.begin(), test_case.argv.end(),
-                     std::back_inserter(cmd_argv), [&](std::string val) {
-                       return TestValkeyModule_CreateStringPrintf(
-                           &fake_ctx_, "%s", val.data());
-                     });
-      EXPECT_EQ(vmsdk::CreateCommand<FTInfoCmd>(&fake_ctx_, cmd_argv.data(),
-                                                cmd_argv.size()),
-                VALKEYMODULE_OK);
-      EXPECT_EQ(fake_ctx_.reply_capture.GetReply(), test_case.expected_output);
+        // Run the command.
+        std::vector<ValkeyModuleString *> cmd_argv;
+        std::transform(test_case.argv.begin(), test_case.argv.end(),
+                       std::back_inserter(cmd_argv), [&](std::string val) {
+                         return TestValkeyModule_CreateStringPrintf(
+                             &fake_ctx_, "%s", val.data());
+                       });
+        EXPECT_EQ(vmsdk::CreateCommand<FTInfoCmd>(&fake_ctx_, cmd_argv.data(),
+                                                  cmd_argv.size()),
+                  VALKEYMODULE_OK);
+        EXPECT_EQ(fake_ctx_.reply_capture.GetReply(),
+                  score_info_fixed
+                      ? std::string(test_case.expected_output)
+                      : ToLegacyScoreInfoShape(test_case.expected_output));
 
-      if (test_case.index_schema_pbtxt.has_value()) {
-        VMSDK_EXPECT_OK(SchemaManager::Instance().RemoveIndexSchema(
-            index_schema_proto.db_num(), index_schema_proto.name()));
-        EXPECT_EQ(SchemaManager::Instance().GetNumberOfIndexSchemas(), 0);
-      }
+        if (test_case.index_schema_pbtxt.has_value()) {
+          VMSDK_EXPECT_OK(SchemaManager::Instance().RemoveIndexSchema(
+              index_schema_proto.db_num(), index_schema_proto.name()));
+          EXPECT_EQ(SchemaManager::Instance().GetNumberOfIndexSchemas(), 0);
+        }
 
-      // Clean up
-      for (auto cmd_arg : cmd_argv) {
-        TestValkeyModule_FreeString(&fake_ctx_, cmd_arg);
+        // Clean up
+        for (auto cmd_arg : cmd_argv) {
+          TestValkeyModule_FreeString(&fake_ctx_, cmd_arg);
+        }
       }
     }
   }
4f5e5642899b2d11a4519f44cb6f8f24e0f9a3eb
Su Ko <rhtn1128@gmail.com>
2026-09-05 08:41:17 +0900

    Fix normalize wildcard string-array values for TAG indexing
    Signed-off-by: Su Ko <rhtn1128@gmail.com>

--- src/indexes/tag.cc
+++ src/indexes/tag.cc
@@ -323,7 +323,7 @@ vmsdk::UniqueValkeyString Tag::NormalizeStringAttribute(
       1, 2, 1, "json_tag_wildcard_array",
       [&] {
         if (!input) {
-          return input;
+          return std::move(input);
         }
         auto normalized =
             JoinJsonStringArray(vmsdk::ToStringView(input.get()), separator_);

e7e60037bb29334a3c29825021b11dfd3ce1feb2
bandalgomsu <rhtn1128@gmail.com>
2026-09-10 07:25:13 +0900

    Reapplies indexing for zero-length keys(#1171)  (#1310)
    Reapplies #1171 and stabilizes zero-length vector save/restore coverage.
    Wait for initial backfill and KNN indexing to complete before saving.
    
    ---------
    
    Signed-off-by: Su Ko <rhtn1128@gmail.com>
    Signed-off-by: Brennan <31714723+BCathcart@users.noreply.github.com>
    Co-authored-by: Brennan <31714723+BCathcart@users.noreply.github.com>

--- src/indexes/vector_base.cc
+++ src/indexes/vector_base.cc
@@ -337,9 +337,6 @@ void VectorBase::RemoveRecordDueToError(const InternedStringPtr &key,
 
 absl::StatusOr<std::optional<uint64_t>> VectorBase::UnTrackKey(
     const InternedStringPtr &key) {
-  if (key->Str().empty()) {
-    return std::nullopt;
-  }
   absl::WriterMutexLock lock(&key_to_metadata_mutex_);
   auto it = tracked_metadata_by_key_.find(key);
   if (it == tracked_metadata_by_key_.end()) {
@@ -359,9 +356,6 @@ absl::StatusOr<std::optional<uint64_t>> VectorBase::UnTrackKey(
 
 absl::StatusOr<uint64_t> VectorBase::TrackKey(const InternedStringPtr &key,
                                               float magnitude) {
-  if (key->Str().empty()) {
-    return absl::InvalidArgumentError("key can't be empty");
-  }
   absl::WriterMutexLock lock(&key_to_metadata_mutex_);
   auto id = inc_id_++;
   auto [_, succ] = tracked_metadata_by_key_.insert(
@@ -378,9 +372,6 @@ absl::StatusOr<uint64_t> VectorBase::TrackKey(const InternedStringPtr &key,
 absl::StatusOr<bool> VectorBase::UpdateMetadata(
     const InternedStringPtr &key, float magnitude,
     const VectorRecord *vector_record) {
-  if (key->Str().empty()) {
-    return absl::InvalidArgumentError("key can't be empty");
-  }
   absl::ReaderMutexLock lock(&resize_mutex_);
   const VectorRecord *stored_record;
   {
